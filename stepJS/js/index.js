dfdb413db72f6f44516671f7d45b7d7b311efd21
Ruslan Teplenko <ruslanteplenko15@gmail.com>
2026-03-09 21:27:36 +0200

    first commit

---
+++ stepJS/js/index.js
@@ -0,0 +1,372 @@
+"use strict";
+
+const DATA = [
+	{
+		"first name": "Олексій",
+		"last name": "Петров",
+		photo: "./img/trainers/trainer-m1.jpg",
+		specialization: "Басейн",
+		category: "майстер",
+		experience: "8 років",
+		description:
+			"Олексій має багаторічний досвід роботи з плавцями. Він займається якісною підготовкою спортсменів на міжнародних змаганнях. Його методика базується на новітніх технологіях тренувань.",
+	},
+	{
+		"first name": "Марина",
+		"last name": "Іванова",
+		photo: "./img/trainers/trainer-f1.png",
+		specialization: "Тренажерний зал",
+		category: "спеціаліст",
+		experience: "2 роки",
+		description:
+			"Марина спеціалізується на роботі з ваговими тренажерами. Вона розробила унікальну програму для набору м'язової маси. Її клієнти завжди задоволені результатами.",
+	},
+	{
+		"first name": "Ігор",
+		"last name": "Сидоренко",
+		photo: "./img/trainers/trainer-m2.jpg",
+		specialization: "Дитячий клуб",
+		category: "інструктор",
+		experience: "1 рік",
+		description:
+			"Ігор працює з дітьми різного віку. Він створив ігрові методики для розвитку координації та спритності. Його уроки завжди цікаві та корисні для малюків.",
+	},
+	{
+		"first name": "Тетяна",
+		"last name": "Мороз",
+		photo: "./img/trainers/trainer-f2.jpg",
+		specialization: "Бійцівський клуб",
+		category: "майстер",
+		experience: "10 років",
+		description:
+			"Тетяна є експертом в бойових мистецтвах. Вона проводить тренування для професіоналів і новачків. Її підхід до навчання допомагає спортсменам досягати високих результатів.",
+	},
+	{
+		"first name": "Сергій",
+		"last name": "Коваленко",
+		photo: "./img/trainers/trainer-m3.jpg",
+		specialization: "Тренажерний зал",
+		category: "інструктор",
+		experience: "1 рік",
+		description:
+			"Сергій фокусується на роботі з фітнесом та кардіотренуваннями. Він вдосконалив свої методики протягом багатьох років. Його учні завжди в формі та енергійні.",
+	},
+	{
+		"first name": "Олена",
+		"last name": "Лисенко",
+		photo: "./img/trainers/trainer-f3.jpg",
+		specialization: "Басейн",
+		category: "спеціаліст",
+		experience: "4 роки",
+		description:
+			"Олена спеціалізується на синхронному плаванні. Вона тренує команди різного рівня. Її команди завжди займають призові місця на змаганнях.",
+	},
+	{
+		"first name": "Андрій",
+		"last name": "Волков",
+		photo: "./img/trainers/trainer-m4.jpg",
+		specialization: "Бійцівський клуб",
+		category: "інструктор",
+		experience: "1 рік",
+		description:
+			"Андрій має досвід у вивченні різних бойових мистецтв. Він викладає техніку та тактику бою. Його учні здобувають перемоги на міжнародних турнірах.",
+	},
+	{
+		"first name": "Наталія",
+		"last name": "Романенко",
+		photo: "./img/trainers/trainer-f4.jpg",
+		specialization: "Дитячий клуб",
+		category: "спеціаліст",
+		experience: "3 роки",
+		description:
+			"Наталія розробила унікальну програму для дітей дошкільного віку. Вона допомагає дітям розвивати фізичні та ментальні навички. Її класи завжди веселі та динамічні.",
+	},
+	{
+		"first name": "Віталій",
+		"last name": "Козлов",
+		photo: "./img/trainers/trainer-m5.jpg",
+		specialization: "Тренажерний зал",
+		category: "майстер",
+		experience: "10 років",
+		description:
+			"Віталій спеціалізується на функціональному тренуванні. Він розробив ряд ефективних тренувальних програм. Його клієнти швидко досягають бажаних результатів.",
+	},
+	{
+		"first name": "Юлія",
+		"last name": "Кравченко",
+		photo: "./img/trainers/trainer-f5.jpg",
+		specialization: "Басейн",
+		category: "спеціаліст",
+		experience: "4 роки",
+		description:
+			"Юлія є експертом у водних видах спорту. Вона проводить тренування з аквагімнастики та аеробіки. Її учні демонструють вражаючі показники на змаганнях.",
+	},
+	{
+		"first name": "Олег",
+		"last name": "Мельник",
+		photo: "./img/trainers/trainer-m6.jpg",
+		specialization: "Бійцівський клуб",
+		category: "майстер",
+		experience: "12 років",
+		description:
+			"Олег є визнаним майстром в бойових мистецтвах. Він тренує чемпіонів різних вагових категорій. Його методики вважаються одними з найефективніших у світі бойових мистецтв.",
+	},
+	{
+		"first name": "Лідія",
+		"last name": "Попова",
+		photo: "./img/trainers/trainer-f6.jpg",
+		specialization: "Дитячий клуб",
+		category: "інструктор",
+		experience: "1 рік",
+		description:
+			"Лідія має великий досвід у роботі з дітьми. Вона організовує різноманітні спортивні ігри та заняття. Її класи завжди допомагають дітям розвивати соціальні навички та командний дух.",
+	},
+	{
+		"first name": "Роман",
+		"last name": "Семенов",
+		photo: "./img/trainers/trainer-m7.jpg",
+		specialization: "Тренажерний зал",
+		category: "спеціаліст",
+		experience: "2 роки",
+		description:
+			"Роман є експертом у кросфіту та функціональних тренуваннях. Він розробив власні програми для різних вікових груп. Його учні часто отримують нагороди на фітнес-турнірах.",
+	},
+	{
+		"first name": "Анастасія",
+		"last name": "Гончарова",
+		photo: "./img/trainers/trainer-f7.jpg",
+		specialization: "Басейн",
+		category: "інструктор",
+		experience: "1 рік",
+		description:
+			"Анастасія фокусується на водних програмах для здоров'я та фітнесу. Вона проводить тренування для осіб з різним рівнем підготовки. Її учні відзначають покращення здоров'я та благополуччя після занять.",
+	},
+	{
+		"first name": "Валентин",
+		"last name": "Ткаченко",
+		photo: "./img/trainers/trainer-m8.jpg",
+		specialization: "Бійцівський клуб",
+		category: "спеціаліст",
+		experience: "2 роки",
+		description:
+			"Валентин є експертом в таеквондо та кікбоксингу. Він викладає техніку, тактику та стратегію бою. Його учні часто стають чемпіонами на національних та міжнародних змаганнях.",
+	},
+	{
+		"first name": "Лариса",
+		"last name": "Петренко",
+		photo: "./img/trainers/trainer-f8.jpg",
+		specialization: "Дитячий клуб",
+		category: "майстер",
+		experience: "7 років",
+		description:
+			"Лариса розробила комплексну програму для розвитку фізичних та інтелектуальних навичок дітей. Вона проводить заняття в ігровій формі. Її методика допомагає дітям стати активними та розумними.",
+	},
+	{
+		"first name": "Олексій",
+		"last name": "Петров",
+		photo: "./img/trainers/trainer-m9.jpg",
+		specialization: "Басейн",
+		category: "майстер",
+		experience: "11 років",
+		description:
+			"Олексій має багаторічний досвід роботи з плавцями. Він займається якісною підготовкою спортсменів на міжнародних змаганнях. Його методика базується на новітніх технологіях тренувань.",
+	},
+	{
+		"first name": "Марина",
+		"last name": "Іванова",
+		photo: "./img/trainers/trainer-f9.jpg",
+		specialization: "Тренажерний зал",
+		category: "спеціаліст",
+		experience: "2 роки",
+		description:
+			"Марина спеціалізується на роботі з ваговими тренажерами. Вона розробила унікальну програму для набору м'язової маси. Її клієнти завжди задоволені результатами.",
+	},
+	{
+		"first name": "Ігор",
+		"last name": "Сидоренко",
+		photo: "./img/trainers/trainer-m10.jpg",
+		specialization: "Дитячий клуб",
+		category: "інструктор",
+		experience: "1 рік",
+		description:
+			"Ігор працює з дітьми різного віку. Він створив ігрові методики для розвитку координації та спритності. Його уроки завжди цікаві та корисні для малюків.",
+	},
+	{
+		"first name": "Наталія",
+		"last name": "Бондаренко",
+		photo: "./img/trainers/trainer-f10.jpg",
+		specialization: "Бійцівський клуб",
+		category: "майстер",
+		experience: "8 років",
+		description:
+			"Наталія є майстром у бойових мистецтвах. Вона вивчала різні техніки та стили, із якими працює зі своїми учнями. Її підхід до навчання відповідає найвищим стандартам.",
+	},
+	{
+		"first name": "Андрій",
+		"last name": "Семенов",
+		photo: "./img/trainers/trainer-m11.jpg",
+		specialization: "Тренажерний зал",
+		category: "інструктор",
+		experience: "1 рік",
+		description:
+			"Андрій спеціалізується на функціональному тренуванні. Він розробив власну систему вправ для зміцнення корпусу. Його учні завжди отримують видимі результати.",
+	},
+	{
+		"first name": "Софія",
+		"last name": "Мельник",
+		photo: "./img/trainers/trainer-f11.jpg",
+		specialization: "Басейн",
+		category: "спеціаліст",
+		experience: "6 років",
+		description:
+			"Софія працює з аквагімнастикою. Вона вивчила різні техніки та стили плавання. Її заняття допомагають клієнтам розслабитися та покращити фізичну форму.",
+	},
+	{
+		"first name": "Дмитро",
+		"last name": "Ковальчук",
+		photo: "./img/trainers/trainer-m12.png",
+		specialization: "Дитячий клуб",
+		category: "майстер",
+		experience: "10 років",
+		description:
+			"Дмитро спеціалізується на розвитку дитячого спорту. Він розробив унікальну програму для малюків. Його методики забезпечують гармонійний розвиток дітей.",
+	},
+	{
+		"first name": "Олена",
+		"last name": "Ткаченко",
+		photo: "./img/trainers/trainer-f12.jpg",
+		specialization: "Бійцівський клуб",
+		category: "спеціаліст",
+		experience: "5 років",
+		description:
+			"Олена є відомим тренером у жіночому бойовому клубі. Вона вивчила різні техніки самооборони. Її підхід дозволяє її ученицям відчувати себе впевнено в будь-яких ситуаціях.",
+	},
+];
+
+const translateData = {
+	"gym" : "Тренажерний зал",
+	"fight club" : "Бійцівський клуб",
+	"kids club" : "Дитячий клуб",
+	"swimming pool" : "Басейн",
+	"instructor" : "інструктор",
+	"specialist" :  "спеціаліст",
+	"master" : "майстер"
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+	const originalData = [...DATA]
+	const trainerContainer = document.querySelector('.trainers-cards__container')
+	function showCards(trainers){
+		trainerContainer.innerHTML = ' '
+		trainers.forEach(trainerCharacter =>{
+		const trainerTemplateCards = document.getElementById('trainer-card')
+		const cardsTemplateContent = trainerTemplateCards.content.cloneNode(true)
+		const trainerImage = cardsTemplateContent.querySelector('.trainer__img')
+		const trainerInfo = cardsTemplateContent.querySelector('.trainer__name')
+		const moreInfo = cardsTemplateContent.querySelector('.trainer__show-more')
+		trainerImage.src = trainerCharacter.photo
+		trainerInfo.textContent = trainerCharacter["last name"] + ' ' + trainerCharacter["first name"]
+		trainerContainer.append(cardsTemplateContent)
+
+		moreInfo.addEventListener('click', () => {
+      		showModal(trainerCharacter)
+    	})
+		trainerContainer.append(cardsTemplateContent);
+		})
+	}
+	function showModal(trainersModal){
+				const modalTemplate = document.getElementById('modal-template')
+				const templateContent = modalTemplate.content.cloneNode(true)
+				const templateContainer = templateContent.querySelector('.modal')
+				const trainerTemplateName = templateContent.querySelector('.modal__name')
+				const trainerTemplateCategory = templateContent.querySelector('.modal__point--category')
+				const trainerTemplateExperience = templateContent.querySelector('.modal__point--experience')
+				const trainerTemplateSpecialization = templateContent.querySelector('.modal__point--specialization')
+				const trainerTemplateText = templateContent.querySelector('.modal__text')
+				const trainerTemplateImage = templateContent.querySelector('.modal__img')
+				const modalCloseButton = templateContent.querySelector('.modal__close')
+				trainerTemplateName.textContent = trainersModal["last name"] + ' ' + trainersModal["first name"]
+				trainerTemplateCategory.textContent = 'Категорія: ' + ' ' + trainersModal.category
+				trainerTemplateExperience.textContent = 'Досвід: ' + ' ' + trainersModal.experience
+				trainerTemplateSpecialization.textContent = 'Напрям тренера: ' + ' ' + trainersModal.specialization
+				trainerTemplateText.textContent  = trainersModal.description
+				trainerTemplateImage.src = trainersModal.photo
+				document.body.style.overflow = 'hidden'
+		
+				modalCloseButton.addEventListener('click',()=>{
+					templateContainer.style.display = 'none'
+					document.body.style.overflow = 'auto'
+				})
+				trainerContainer.append(templateContent)
+	}
+	
+	showCards(DATA)
+
+	const sortTrainers = document.querySelector('.sorting')
+	sortTrainers.removeAttribute('hidden')
+	const filterBar = document.querySelector('.sidebar')
+	filterBar.removeAttribute('hidden')
+
+
+	const sortButton = document.querySelectorAll('.sorting__btn')
+		sortButton[0].addEventListener('click',()=>{
+				sortButton.forEach(button =>{
+					button.classList.remove('sorting__btn--active')
+					sortButton[0].classList.add('sorting__btn--active')
+				})	
+			showCards(originalData)
+
+		})
+		sortButton[1].addEventListener('click',()=>{
+				sortButton.forEach(button =>{
+					button.classList.remove('sorting__btn--active')
+					sortButton[1].classList.add('sorting__btn--active')
+				})	
+				const sortedBySurname = [...DATA].sort((a,b)=>{
+				const surnameFirstLetter = a["last name"].split(' ')[0]
+				const surnameSecondLetter = b["last name"].split(' ')[0]
+				return surnameFirstLetter.localeCompare(surnameSecondLetter, 'uk')
+			})
+			showCards(sortedBySurname)
+			 
+		})
+		sortButton[2].addEventListener('click',()=>{
+				sortButton.forEach(button =>{
+					button.classList.remove('sorting__btn--active')
+					sortButton[2].classList.add('sorting__btn--active')
+				})	
+				const sortedByExperiance = [...DATA].sort((a,b)=>{
+					return parseInt(b.experience) - parseInt(a.experience)
+				})
+				showCards(sortedByExperiance)
+					
+		})
+
+		const showButton = document.querySelector('.filters__submit')
+		function filterData(translated){
+			showButton.addEventListener('click',(event)=>{
+			event.preventDefault()
+			const radioButDirection = document.querySelector('input[name="direction"]:checked').value
+			const radioButCategory = document.querySelector('input[name="category"]:checked').value
+			const filteredData = DATA.filter(trainer => {
+				const translatedDirection = translated[radioButDirection];
+				const translatedCategory = translated[radioButCategory];
+
+				const matchDirection = radioButDirection === 'all' || trainer.specialization === translatedDirection;
+				const matchCategory = radioButCategory === 'all' || trainer.category === translatedCategory;
+
+				return matchDirection && matchCategory;
+			})
+			showCards(filteredData)	
+			
+		})	
+		}
+		filterData(translateData)
+
+		
+		
+		
+		
+
+	
+})
